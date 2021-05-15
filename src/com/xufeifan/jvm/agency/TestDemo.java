20b48441c81870a19415fab54eb0f29a9fb17e93
DZRABDemo <1156599543@qq.com>
2021-05-15 11:39:34 +0800

    this is one test

---
+++ src/com/xufeifan/jvm/agency/TestDemo.java
@@ -0,0 +1,17 @@
+package com.xufeifan.jvm.agency;
+
+/**
+ * @ProjectName: JVMDemoStart
+ * @Package: com.xufeifan.jvm.agency
+ * @Description: java类作用描述
+ * @Author: 徐非凡
+ * @CreateDate: 2021/5/15 11:37
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2021
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
